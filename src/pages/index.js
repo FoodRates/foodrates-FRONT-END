3d1ef9db3b5d0b602a8fb02977ab999b334f2f4f
Bakytbek Tatibekov <bakytbek.tatibekov@gmail.com>
2023-02-17 22:58:14 -0800

    refactored the main landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,27 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+          <p>Welcome to Foodrates</p>
         </div>
 
         <div className={styles.center}>
@@ -60,40 +40,16 @@ export default function Home() {
         </div>
 
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className={styles.card}>
             <h2 className={inter.className}>
               Purpose <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Create a fast rendering product that does not suck
+              Create a fast rendering product that does not suck.
             </p>
           </a>
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className={styles.card}>
             <h2 className={inter.className}>
               Learn <span>-&gt;</span>
             </h2>
@@ -102,12 +58,7 @@ export default function Home() {
             </p>
           </a>
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className={styles.card}>
             <h2 className={inter.className}>
               Templates <span>-&gt;</span>
             </h2>
@@ -116,12 +67,7 @@ export default function Home() {
             </p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className={styles.card}>
             <h2 className={inter.className}>
               Deploy <span>-&gt;</span>
             </h2>
---
+++ src/pages/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <p>This is a Main landing page.</p>
+    </div>
+  );
+};
+
+export default Main;
