03522c3cdb98bff7d247e862e76e9be817189b32
Bakytbek Tatibekov <bakytbek.tatibekov@gmail.com>
2023-02-18 16:06:10 -0800

    main page, css file created

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "@/styles/Main.module.css";
+
+const URL =
+  "https://ixuwvr2560.execute-api.us-west-1.amazonaws.com/dev/allmenus";
 
 const Main = () => {
+  const [data, setData] = useState(null);
+
+  const fetchMenus = async () => {
+    try {
+      const response = await axios.get(URL);
+
+      setData(response.data);
+    } catch (error) {
+      console.log("Error occurred :(", error);
+      throw error;
+    }
+  };
+  useEffect(() => {
+    fetchMenus();
+  }, []);
   return (
-    <div>
+    <main className={styles.main}>
+      {data && console.log("data here: ", data)}
       <p>This is a Main landing page.</p>
-    </div>
+    </main>
   );
 };
 
